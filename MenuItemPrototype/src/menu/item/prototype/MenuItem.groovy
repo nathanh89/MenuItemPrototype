f3881ca1ed4f06339ebeac607e9730a7c48c276b
Nathan Hutcheson <nathanuhutcheson@gmail.com>
2012-04-25 17:05:52 -0700

    Created MenuItem and Recipe classes, verified instantiation

---
+++ MenuItemPrototype/src/menu/item/prototype/MenuItem.groovy
@@ -0,0 +1,11 @@
+package menu.item.prototype
+
+class MenuItem {
+	
+	def name
+	def price
+	def recipe
+	def picture
+	def QR
+
+}
---
+++ MenuItemPrototype/src/menu/item/prototype/Recipe.groovy
@@ -0,0 +1,14 @@
+package menu.item.prototype
+
+class Recipe {
+	
+	def name
+	def tools
+	def ingredients
+	def prepTime
+	def preparationSteps
+
+	def addTool(String tool){
+		tools += tool
+	}
+}
